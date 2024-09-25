b6cd2e78b5a0c2ed258a26ba91cd54b30edf3fd7
Niels Drost <nielsdrost7@gmail.com>
2024-09-25 03:40:16 -0700

    pint styling

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -23,7 +23,9 @@ class EventServiceProvider extends ServiceProvider
     /**
      * Register any events for your application.
      */
-    public function boot(): void {}
+    public function boot(): void
+    {
+    }
 
     /**
      * Determine if events and listeners should be automatically discovered.
