d60a5ff181d2dc042d85bb99ac3bb334508eccf0
Niels Drost <nielsdrost7@gmail.com>
2024-09-26 09:25:36 -0700

    Improved Payment Create/Edit form

--- Modules/Payments/Filament/Resources/PaymentResource.php
+++ Modules/Payments/Filament/Resources/PaymentResource.php
@@ -2,6 +2,12 @@
 
 namespace Modules\Payments\Filament\Resources;
 
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
@@ -39,6 +45,45 @@ class PaymentResource extends Resource
     {
         return $form
             ->schema([
+                Group::make()
+                    ->schema([
+                        Section::make(heading:null)
+                            ->schema([
+                                Select::make('invoice_id')
+                                    ->required()
+                                    ->relationship('invoice', 'invoice_number')
+                                    ->searchable()
+                                    ->preload()
+                                    ->native(false)
+                                    ->disabled(fn (?Payment $record) => $record !== null)
+                                    ->extraAttributes(
+                                        fn (?Payment $record) => $record !== null ? ['class' => 'disabled-select'] : []
+                                    ),
+                                TextInput::make('payment_amount'),
+                            ])->columns(1),
+                        Section::make(heading:null)
+                            ->schema([
+                                MarkdownEditor::make('payment_note')
+                                    ->toolbarButtons([
+                                        'bold',
+                                        'italic',
+                                    ])
+                                    ->columnSpan('full'),
+                            ])->columns(1),
+                    ]),
+                Group::make()
+                    ->schema([
+                        Section::make(heading:null)
+                            ->schema(components: [
+                                DatePicker::make('payment_date'),
+                                Select::make('payment_method_id')
+                                    ->required()
+                                    ->relationship('paymentMethod', 'payment_method_name')
+                                    ->searchable()
+                                    ->preload()
+                                    ->native(false),
+                            ]),
+                    ]),
             ]);
     }
 
@@ -47,17 +92,11 @@ class PaymentResource extends Resource
         return $table
             ->columns([
                 TextColumn::make('payment_date'),
-
                 TextColumn::make('invoice.invoice_number'),
-
                 TextColumn::make('invoice.invoiceGroup.invoice_group_name')->hiddenFrom('sm'),
-
                 TextColumn::make('invoice.invoice_date_due'),
-
                 TextColumn::make('invoice.client.client_name'),
-
                 TextColumn::make('payment_amount'),
-
                 TextColumn::make('paymentMethod.payment_method_name')->hiddenFrom('sm'),
             ])
             ->filters([
