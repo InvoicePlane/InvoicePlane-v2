91df9acabdd1bab6bd0b681996a184e33b882188
Niels Drost <nielsdrost7@gmail.com>
2024-12-29 14:07:46 +0100

    Fix tests for Clients

---
+++ Modules/Clients/Filament/Resources/ClientResource/Pages/CreateClient.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Modules\Clients\Filament\Resources\ClientResource\Pages;
+
+use Filament\Resources\Pages\CreateRecord;
+use Modules\Clients\Filament\Resources\ClientResource;
+
+class CreateClient extends CreateRecord
+{
+    protected static string $resource = ClientResource::class;
+
+    public function create(bool $another = false): void
+    {
+        $this->form->fill(array_merge(
+            $this->form->getRawState(),
+            [
+                'client_date_created'  => now()->toDateTimeString(),
+                'client_date_modified' => now()->toDateTimeString(),
+            ]
+        ));
+
+        parent::create($another);
+    }
+}
---
+++ Modules/Clients/Filament/Resources/ClientResource/Pages/EditClient.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Modules\Clients\Filament\Resources\ClientResource\Pages;
+
+use Filament\Resources\Pages\EditRecord;
+use Modules\Clients\Filament\Resources\ClientResource;
+
+class EditClient extends EditRecord
+{
+    protected static string $resource = ClientResource::class;
+
+    public function save(bool $shouldRedirect = true, bool $shouldSendSavedNotification = true): void
+    {
+        $this->form->fill(array_merge(
+            $this->form->getRawState(),
+            [
+                'client_date_modified' => now()->toDateTimeString(),
+            ]
+        ));
+
+        parent::save();
+    }
+}
