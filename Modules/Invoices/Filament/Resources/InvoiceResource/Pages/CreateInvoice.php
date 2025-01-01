aac3693f0ee75e5bbf5ec51cb7d9de0d27c0b595
Niels Drost <nielsdrost7@gmail.com>
2025-01-01 13:09:30 +0100

    Final fixes

--- Modules/Invoices/Filament/Resources/InvoiceResource/Pages/CreateInvoice.php
+++ Modules/Invoices/Filament/Resources/InvoiceResource/Pages/CreateInvoice.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Modules\Core\Filament\Resources\UserResource\Pages;
+namespace Modules\Invoices\Filament\Resources\InvoiceResource\Pages;
 
 use Filament\Resources\Pages\ManageRecords;
 use Modules\Core\Filament\Resources\UserResource;
