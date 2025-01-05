52bc2841f503d8a6926985a2bc94f17568cfc37c
Niels Drost <nielsdrost7@gmail.com>
2025-01-05 13:52:23 +0100

    phpStatus Enum

--- Modules/Payments/Enums/PaymentStatus.php
+++ Modules/Payments/Enums/PaymentStatus.php
@@ -2,29 +2,25 @@
 
 namespace Modules\Payments\Enums;
 
-use Exception;
-
 enum PaymentStatus: string
 {
-    public const STATUS_REFUNDED_PARTIALLY = 'ip.partially_refunded';
-
-    public const STATUS_FAILED = 'ip.failed';
+    case REFUNDED_PARTIALLY = 'ip.partially_refunded';
+    case FAILED = 'ip.failed';
 
     public function getLabel(): string
     {
-        return match($this) {
-            self::STATUS_REFUNDED_PARTIALLY => 'ip.partially_refunded',
-            self::STATUS_FAILED             => 'ip.failed',
-            default                         => throw new Exception('Unexpected match value'),
+        return match ($this) {
+            self::REFUNDED_PARTIALLY => 'ip.partially_refunded',
+            self::FAILED             => 'ip.failed',
         };
     }
 
     public function getColor(): string
     {
-        return match($this) {
-            self::STATUS_REFUNDED_PARTIALLY => 'warning',
-            self::STATUS_FAILED             => 'maroon',
-            default                         => 'gray',
+        return match ($this) {
+            self::REFUNDED_PARTIALLY => 'warning',
+            self::FAILED             => 'maroon',
+            default                  => 'gray',
         };
     }
 }
