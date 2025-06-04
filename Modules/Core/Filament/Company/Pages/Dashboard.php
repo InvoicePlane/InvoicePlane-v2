4d4c6509a528adf5dfb79d2c8454d1b5225f5083
Niels Drost <nielsdost7+github@gmail.com>
2025-06-04 10:04:28 +0200

    Custom Dashboard same as Base Dashboard

--- Modules/Core/Filament/Company/Pages/Dashboard.php
+++ Modules/Core/Filament/Company/Pages/Dashboard.php
@@ -2,6 +2,22 @@
 
 namespace Modules\Core\Filament\Company\Pages;
 
-use Filament\Pages\Dashboard as BaseDashboard;
+use Filament\Pages\Page;
+use Filament\Widgets\AccountWidget;
+use Filament\Widgets\FilamentInfoWidget;
 
-class Dashboard extends BaseDashboard {}
+class Dashboard extends Page
+{
+    public static function getSlug(): string
+    {
+        return 'dashboard';
+    }
+
+    public function getHeaderWidgets(): array
+    {
+        return [
+            AccountWidget::class,
+            FilamentInfoWidget::class,
+        ];
+    }
+}
