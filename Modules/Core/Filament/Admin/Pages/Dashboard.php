e651cb9afd3d787fbefe7d8b29d6f566291e1e53
Niels Drost <nielsdost7+github@gmail.com>
2025-06-04 10:04:34 +0200

    Custom Dashboard same as Base Dashboard

--- Modules/Core/Filament/Admin/Pages/Dashboard.php
+++ Modules/Core/Filament/Admin/Pages/Dashboard.php
@@ -4,4 +4,12 @@ namespace Modules\Core\Filament\Admin\Pages;
 
 use Filament\Pages\Dashboard as BaseDashboard;
 
-class Dashboard extends BaseDashboard {}
+class Dashboard extends BaseDashboard
+{
+    protected static bool $isScopedToTenant = false;
+
+    public static function getSlug(): string
+    {
+        return 'dashboard';
+    }
+}
