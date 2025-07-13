519d8eb8c82eb8e2a7e26bd3f72016179cd24984
Niels Drost <nielsdrost7@gmail.com>
2025-07-13 08:04:15 +0200

    Improved Dashboard to match Filament v4 style

--- Modules/Core/Filament/Admin/Pages/Dashboard.php
+++ Modules/Core/Filament/Admin/Pages/Dashboard.php
@@ -3,12 +3,13 @@
 namespace Modules\Core\Filament\Admin\Pages;
 
 use Filament\Pages\Dashboard as BaseDashboard;
+use Filament\Panel;
 
 class Dashboard extends BaseDashboard
 {
     protected static bool $isScopedToTenant = false;
 
-    public static function getSlug(): string
+    public static function getSlug(?Panel $panel = null): string
     {
         return 'dashboard';
     }
--- Modules/Core/Filament/Company/Pages/Dashboard.php
+++ Modules/Core/Filament/Company/Pages/Dashboard.php
@@ -3,12 +3,13 @@
 namespace Modules\Core\Filament\Company\Pages;
 
 use Filament\Pages\Page;
+use Filament\Panel;
 use Filament\Widgets\AccountWidget;
 use Filament\Widgets\FilamentInfoWidget;
 
 class Dashboard extends Page
 {
-    public static function getSlug(): string
+    public static function getSlug(?Panel $panel = null): string
     {
         return 'dashboard';
     }
