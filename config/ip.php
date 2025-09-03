3722aec320442db112d1b730c21fe1045fb5a668
Niels Drost <nielsdrost7@gmail.com>
2025-09-03 09:11:28 +0200

    language file and config file

---
+++ config/ip.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    'date_formats' => [
+        'd/m/Y' => date('d/m/Y') . ' (d/m/Y)',
+        'd-m-Y' => date('d-m-Y') . ' (d-m-Y)',
+        'd-M-Y' => date('d-M-Y') . ' (d-M-Y)',
+        'd.m.Y' => date('d.m.Y') . ' (d.m.Y)',
+        'j.n.Y' => date('j.n.Y') . ' (j.n.Y)',
+        'd M,Y' => date('d M,Y') . ' (d M,Y)',
+        'm/d/Y' => date('m/d/Y') . ' (m/d/Y)',
+        'm-d-Y' => date('m-d-Y') . ' (m-d-Y)',
+        'm.d.Y' => date('m.d.Y') . ' (m.d.Y)',
+        'Y/m/d' => date('Y/m/d') . ' (Y/m/d)',
+        'Y-m-d' => date('Y-m-d') . ' (Y-m-d)',
+        'Y.m.d' => date('Y.m.d') . ' (Y.m.d)',
+    ],
+    'default_decimals_for_items' => [
+        '1' => '1',
+        '2' => '2',
+        '3' => '3',
+        '4' => '4',
+        '5' => '5',
+        '6' => '6',
+        '7' => '7',
+        '8' => '8',
+    ],
+    'number_of_items_in_list' => [
+        '15'  => '15',  // <<== for legacy purposes
+        '25'  => '25',
+        '50'  => '50',
+        '100' => '100',
+        '250' => '250',
+    ],
+    'tax_rate_decimal_places' => [
+        '2' => '2',
+        '3' => '3',
+    ],
+];
