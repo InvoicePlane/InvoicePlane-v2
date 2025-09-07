9389692186f399e23bd6bba69996e19bbe9d19ff
Niels Drost <nielsdrost7@gmail.com>
2025-09-07 13:10:55 +0200

    Intermediary commits for the exports refs #85

--- Modules/Quotes/Filament/Company/Resources/Quotes/Pages/ListQuotes.php
+++ Modules/Quotes/Filament/Company/Resources/Quotes/Pages/ListQuotes.php
@@ -28,18 +28,22 @@ class ListQuotes extends ListRecords
                 ->modalWidth('full'),
 
             ActionGroup::make([
-                Action::make('exportCsv')
-                    ->label('Export as CSV')
+                Action::make('exportCsvV2')
+                    ->label('Export as CSV (v2)')
                     ->icon('heroicon-o-document-text')
-                    ->action(function () {
-                        return app(QuoteExportService::class)->export('csv');
-                    }),
-                Action::make('exportExcel')
-                    ->label('Export as Excel')
+                    ->action(fn () => app(QuoteExportService::class)->export('csv')),
+                Action::make('exportCsvV1')
+                    ->label('Export as CSV (v1, Legacy)')
+                    ->icon('heroicon-o-document-text')
+                    ->action(fn () => app(QuoteExportService::class)->exportWithVersion('csv', 1)),
+                Action::make('exportExcelV2')
+                    ->label('Export as Excel (v2)')
                     ->icon('heroicon-o-document')
-                    ->action(function () {
-                        return app(QuoteExportService::class)->export('xlsx');
-                    }),
+                    ->action(fn () => app(QuoteExportService::class)->export('xlsx')),
+                Action::make('exportExcelV1')
+                    ->label('Export as Excel (v1, Legacy)')
+                    ->icon('heroicon-o-document')
+                    ->action(fn () => app(QuoteExportService::class)->exportWithVersion('xlsx', 1)),
             ])
                 ->label('Export')
                 ->icon(Heroicon::OutlinedFolderArrowDown)
--- Modules/Quotes/Services/QuoteExportService.php
+++ Modules/Quotes/Services/QuoteExportService.php
@@ -4,6 +4,7 @@ namespace Modules\Quotes\Services;
 
 use Maatwebsite\Excel\Facades\Excel;
 use Modules\Quotes\Exports\QuotesExport;
+use Modules\Quotes\Exports\QuotesLegacyExport;
 use Modules\Quotes\Models\Quote;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
@@ -11,10 +12,22 @@ class QuoteExportService
 {
     public function export(string $format = 'csv'): BinaryFileResponse
     {
-        $companyId = session('current_company_id');
-        $quotes    = Quote::query()->where('company_id', $companyId)->get();
-        $fileName  = 'quotes-' . now()->format('Y-m-d_H-i-s') . '.' . ($format === 'csv' ? 'csv' : 'xlsx');
+        $companyId   = session('current_company_id');
+        $quotes      = Quote::query()->where('company_id', $companyId)->get();
+        $fileName    = 'quotes-' . now()->format('Y-m-d_H-i-s') . '.' . ($format === 'csv' ? 'csv' : 'xlsx');
+        $version     = config('ip.export_version', 2);
+        $exportClass = $version === 1 ? QuotesLegacyExport::class : QuotesExport::class;
 
-        return Excel::download(new QuotesExport($quotes), $fileName, $format === 'csv' ? Excel::CSV : Excel::XLSX);
+        return Excel::download(new $exportClass($quotes), $fileName, $format === 'csv' ? Excel::CSV : Excel::XLSX);
+    }
+
+    public function exportWithVersion(string $format = 'csv', int $version = 2): BinaryFileResponse
+    {
+        $companyId   = session('current_company_id');
+        $quotes      = Quote::query()->where('company_id', $companyId)->get();
+        $fileName    = 'quotes-' . now()->format('Y-m-d_H-i-s') . '.' . ($format === 'csv' ? 'csv' : 'xlsx');
+        $exportClass = $version === 1 ? QuotesLegacyExport::class : QuotesExport::class;
+
+        return Excel::download(new $exportClass($quotes), $fileName, $format === 'csv' ? Excel::CSV : Excel::XLSX);
     }
 }
