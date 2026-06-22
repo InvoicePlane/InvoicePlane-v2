26ec6b39ed44c64413aa1a5c52261341097f28dd
KhawarMehfooz <khawarmehfooz565@gmail.com>
2026-06-22 20:16:03 +0500

    feat: enforce permissions in Relations (Clients) module (Closes #492)

--- Modules/Clients/Filament/Company/Resources/Relations/RelationResource.php
+++ Modules/Clients/Filament/Company/Resources/Relations/RelationResource.php
@@ -6,10 +6,12 @@ use BackedEnum;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Model;
 use Modules\Clients\Filament\Company\Resources\Relations\Pages\ListRelations;
 use Modules\Clients\Filament\Company\Resources\Relations\Schemas\RelationForm;
 use Modules\Clients\Filament\Company\Resources\Relations\Tables\RelationsTable;
 use Modules\Clients\Models\Relation;
+use Modules\Core\Enums\Permission;
 use Modules\Core\Filament\Company\Resources\BaseResource;
 
 class RelationResource extends BaseResource
@@ -38,5 +40,25 @@ class RelationResource extends BaseResource
         return [
             'index' => ListRelations::route('/'),
         ];
+    }
+
+    public static function canViewAny(): bool
+    {
+        return auth()->user()?->can(Permission::VIEW_RELATIONS->value) ?? false;
+    }
+
+    public static function canCreate(): bool
+    {
+        return auth()->user()?->can(Permission::CREATE_RELATIONS->value) ?? false;
+    }
+
+    public static function canEdit(Model $record): bool
+    {
+        return auth()->user()?->can(Permission::EDIT_RELATIONS->value) ?? false;
+    }
+
+    public static function canDelete(Model $record): bool
+    {
+        return auth()->user()?->can(Permission::DELETE_RELATIONS->value) ?? false;
     }
 }
